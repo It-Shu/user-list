21ca5e612987e89fbe384691946661effc526a2f
It-Shu <it.shustov@gmail.com>
2023-07-26 18:42:16 +0300

    some refactore

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,6 +1,6 @@
 import{ ThunkDispatch } from 'redux-thunk'
-import { applyMiddleware, combineReducers, createStore, Action } from "redux"
-import thunk from "redux-thunk"
+import { applyMiddleware, combineReducers, createStore, Action } from 'redux'
+import thunk from 'redux-thunk'
 import {usersReducer} from '../reducers/usersReducer'
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
--- src/utils/textHighLighting.tsx
+++ src/utils/textHighLighting.tsx
@@ -1,10 +1,11 @@
-import styles from "../components/UserCard/UserCard.module.scss";
-import React from "react";
+import React from 'react';
+import styles from '../components/UserCard/UserCard.module.scss';
+
 
 export const textHighLighting = (text: string, highlight: string) => {
     const parts = text.split(new RegExp(`(${highlight.trim()})`, 'gi'));
     return <span> {parts.map((part, i) =>
-            <span key={i} className={part.toLowerCase() === highlight.trim().toLowerCase() ? styles['highlight'] : undefined}>
+            <span key={i} className={part.toLowerCase() === highlight.trim().toLowerCase() ? styles.highlight : undefined}>
         {part}
         </span>)
     } </span>;
